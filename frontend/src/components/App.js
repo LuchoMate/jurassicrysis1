8f115159a7ed9c3cc30ba1121a600b56374b9bff
Luis Balladares <luisdavidballadares@gmail.com>
2021-03-24 22:45:10 -0300

    basic board layout on html

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,9 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            height : window.innerHeight,
+            width : window.innerWidth
+        }
+
+    }
+
+    componentDidMount() {
+        console.log(this.state.height);
+        console.log(this.state.width);
+        console.log('Hola');
+    }
+
     render() {
-        return <h1>React App</h1>
+        return <h1>React App 2</h1>
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+//ReactDOM.render(<App />, document.getElementById('root'))
