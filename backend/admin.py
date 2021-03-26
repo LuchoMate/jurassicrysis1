2169773108843edd14f77fe89bd24c90aee4e876
Luis Balladares <luisdavidballadares@gmail.com>
2021-03-25 22:44:17 -0300

    added collection field

--- backend/admin.py
+++ backend/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Player, Card
+from .models import Player, Card, Collection
 
 admin.site.register(Player)
 admin.site.register(Card)
+admin.site.register(Collection)
--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
+from django.core.validators import MaxValueValidator
 
 class Player(AbstractUser):
     member_since = models.DateField(auto_now_add=timezone.now())
@@ -33,16 +34,42 @@ class Card(models.Model):
 
     name = models.CharField(max_length=30)
     attack = models.PositiveSmallIntegerField(default = 0)
-    life = models.PositiveSmallIntegerField(default = 0)
+    life_points = models.PositiveSmallIntegerField(default = 0)
     card_type = models.CharField(max_length=2, choices=type_choices, default='ca')
     rarity = models.CharField(max_length=2, choices=rarity_choices, default='co')
     size = models.CharField(max_length=2, choices=size_choices, default='sm')
+    condition_text = models.CharField(max_length=50, blank=True)
     effect_text = models.CharField(max_length=200, blank=True)
     img_src = models.CharField(max_length=200, blank=True)
-    attack_sound = models.CharField(max_length=200, blank=True)
+    intro_sound = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
-        return f"{ self.id} -- {self.name} -- {self.card_type}"
+        return f"{self.name} -- {self.card_type}"
+
+
+#individual cards collected by players
+class Collection(models.Model):
+    Owner = models.ForeignKey(Player, on_delete=models.CASCADE, null=True)
+    Card_collected = models.ForeignKey(Card, on_delete=models.CASCADE, null=True)
+    quantity = models.PositiveSmallIntegerField(default = 1)
+    on_deck = models.PositiveSmallIntegerField(default = 0,
+     validators=[MaxValueValidator(2, 'Maximum 2 per deck')])
+
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['Owner', 'Card_collected'], name='individual_card')
+        ]
+
+    
+
+    def __str__(self):
+        return f"{self.Owner} owns {self.quantity} of {self.Card_collected}"
+
+
+
+
+
 
 
 
