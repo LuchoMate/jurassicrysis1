3971430baf3c609c09e3315eefb955b3f21df1af
Luis Balladares <luisdavidballadares@gmail.com>
2021-04-02 23:02:40 -0300

    transition opacity with barba, excluded logout and play

---
+++ frontend/static/frontend/transitions/transition1.js
@@ -0,0 +1,16 @@
+barba.init({
+    prevent: data => data.el.classList.contains('stopBarba'),
+    transitions: [{
+      name: 'opacity-transition',
+      leave(data) {
+        return gsap.to(data.current.container, {
+          opacity: 0
+        });
+      },
+      enter(data) {
+        return gsap.from(data.next.container, {
+          opacity: 0
+        });
+      }
+    }]
+  });
