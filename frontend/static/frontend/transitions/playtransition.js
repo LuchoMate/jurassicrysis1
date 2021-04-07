c2e44ed9b8c64a7668675e970f3679f234f731f6
Luis Balladares <luisdavidballadares@gmail.com>
2021-04-07 13:21:29 -0400

    choose difficulty empezando

--- frontend/static/frontend/transitions/playtransition.js
+++ frontend/static/frontend/transitions/playtransition.js
@@ -7,14 +7,44 @@ function pageIn() {
         scaleY: 0,
         transformOrigin: "top left",
         ease: "bounce.out"
-        
+    });
+    tl.from("#difficultybox", {
+        duration: 1,
+        y: 300,
+        opacity: 0
+    })
+}
+
+function difficultybg() {
+    var tl = gsap.timeline({repeat:-1});
+    tl.to(".difficultypage", {
+        duration: 30,
+        backgroundPosition: "-2247px 0px",
+        ease: Linear.easeNone
     });
 }
 
+function difficultyOut() {
+    var tl = gsap.timeline();
+    tl.to("#difficultybox", {
+        duration: 1,
+        y: 300,
+        opacity: 0,
+        scaleY: 0
+
+    }).set("#difficultybox", {display: 'none'});
+    return tl;
+}
+
+
+
+difficultybg();
 pageIn();
 
+
+
 function pageOut() {
-    var tl = gsap.timeline();
+    var tl = gsap.timeline();    
 
     tl.to(".plytransition div", {
         duration: 1.5,
